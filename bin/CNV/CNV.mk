2dd6b5ff21484d56d0bf86bc520a744ebff4637c
chengfangtu <1161671154@qq.com>
2024-12-05 11:28:01 +0800

    创建目录结构

---
+++ bin/CNV/CNV.mk
@@ -0,0 +1,29 @@
+bindir=$(dir $(firstword $(MAKEFILE_LIST)))/../
+name=$(notdir $(firstword $(MAKEFILE_LIST)))
+script=$(bindir)/script/
+
+ifeq ($(strip $(config)),)
+Bconfig=$(bindir)/config/config.txt
+else
+Bconfig=$(config)
+endif
+include $(Bconfig)
+
+HELP:
+	@echo Description:
+	@echo 生成单个样本的SNP indel 的*.raw.vcf *.filter.vcf
+	@echo Usage:
+	@echo make -f $(tmpdir)/Saile_SNP_INDEL.mk bam= sample= outdir= ref= ploidy= SELECT Filter extract
+
+.PHONY:CNV
+CNV:
+	echo CNV start at `date`
+	mkdir -p $(outdir)
+	$(cnvkit_env) && cnvkit.py batch $(inbam) -n -m hybrid -t $(bed) --annotate $(flat_file) --fasta $(genome) --access $(access_file) --output-reference $(outdir)/mm10.cnn --output-dir $(outdir) --diagram
+	echo CNV end at `date`
+
+CNV_anno:
+	echo CNV anno start at `date`
+	awk -F "\t" '{print  $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\tGT\t./.\t"$9"\t-\t-\t-"}' $(infile) |sed '1d' - > $(outdir)/$(sample).CNV.raw.vcf
+	$(PERL) $(annovar_perl) $(outdir)/$(sample).CNV.raw.vcf $(annovardb) -outfile $(outdir)/$(sample).CNV -buildver Mus_musculus $(annovar_opt)
+	echo CNV anno end at `date`
---
+++ bin/CNV/script/format.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+import os
+import re
+
+bindir = os.path.abspath(os.path.dirname(__file__))
+__author__='yangyumei'
+__mail__= 'yumeiyang@annoroad.com'
+
+
+pat1=re.compile('^s+$')
+
+def main():
+	parser=argparse.ArgumentParser(description=__doc__,
+			formatter_class=argparse.RawDescriptionHelpFormatter,
+			epilog='author:\t{0}\nmail:\t{1}'.format(__author__,__mail__))
+	parser.add_argument('-i','--infile',help='input file',dest='infile',required=True)
+	parser.add_argument('-o','--outfile',help='outfile',dest='outfile',required=True)
+	args=parser.parse_args()
+	
+	with open( args.infile, 'r') as input, open(args.outfile, 'w') as output:
+		for line in input:
+			tmp = line.rstrip().split('\t')
+			if line.startswith("Chr"):
+				head = ["Chr",'Start',"End","Func.ensGene","Gene.ensGene","GeneDetail.ensGene","ExonicFunc.ensGene","AAChange.ensGene","Gene","CN","Depth"]
+				output.write('\t'.join(head)+'\n')
+				continue
+			out_index = [ 0,1,2,5,6,7,8,9,11,20,23 ]
+			out_value = [tmp[i] for i in out_index]
+			output.write('\t'.join(out_value)+'\n')
+	
+	
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+
